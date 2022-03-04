272f50cd01183bbac7d3f5d895ec4d27c0772a6a
jeroenLu <jeroenluers@gmail.com>
2022-03-04 23:44:55 +0100

    init

---
+++ src/test/java/com/dockerjenkinsrancherintegration/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dockerjenkinsrancherintegration.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
